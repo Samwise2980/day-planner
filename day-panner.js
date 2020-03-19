87ae5483ccc1848d03e7f87f7445398d35db0f67
Samwise <samuelfoster0@gmail.com>
2020-03-18 23:54:35 -0700

    Got the textarea to save the value of what it typed in to it when the save button is pressed. It will restore what was pressed, when the page refreshes.

--- day-panner.js
+++ day-panner.js
@@ -1,73 +1,70 @@
 moment().format();
 
-// GIVEN I am using a daily planner to create a schedule
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// ------------------------------------------------------------------//
-
-// use class for "past", "present", and "future" to apply styles
-
-// Get the current day, months, date from moment.js
-// set the #currentDay to this value
-
-// Set value for current hour/minute/second from moment.js
-
-// If current time is less than set value time
-// Color past
-
-// IF current time is the same as value time
-// Color present
-
-// IF current time is greater than set value time
-// Color future
-
-// When user clicks on the save button
-// Then saves text area value to local storage
-
-// When the page refreshes
-// Get the text string for each hour from local storage
-
-
+// Current times hours in military
 var currentTime = moment().format("HH");
-var hours = [09, 10, 11, 12, 13, 14, 15, 16, 17]
-var dayPlannerID = [$("#hour-9"), $("#hour-10"), $("#hour-11"), $("#hour-12"), $("#hour-13"), $("#hour-14"), $("#hour-15"), $("#hour-16"), $("#hour-17"),]
+// Array of hours 
+var hoursArray = [09, 10, 11, 12, 13, 14, 15, 16, 17]
+// Array of IDs for each hour
+var dayPlannerIDArray = [$("#hour-9"), $("#hour-10"), $("#hour-11"), $("#hour-12"), $("#hour-13"), $("#hour-14"), $("#hour-15"), $("#hour-16"), $("#hour-17")]
 
 // Execute functions
 currentDay();
 hourColorSet();
+getStoredEvents()
+
+// Sets the current day to the top of the page
 function currentDay(){
 
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
   
 }
 
-$(".saveBtn").on("click", function(){
-  alert("Yep, still here!")
-});
-
-
+// Sets the color of the ID if it is the present, future, or past time
 function hourColorSet(){
-  for (i = 0; i < hours.length; i++) {
+  for (i = 0; i < hoursArray.length; i++) {
 
-    if (currentTime == hours[i]) {
-      dayPlannerID[i].addClass("present");
+    if (currentTime == hoursArray[i]) {
+      dayPlannerIDArray[i].addClass("present");
     } 
     
-    else if (currentTime < hours[i]) {
-      dayPlannerID[i].addClass("future");
+    else if (currentTime < hoursArray[i]) {
+      dayPlannerIDArray[i].addClass("future");
     }
     
-    else if (currentTime > hours[i]) {
-      dayPlannerID[i].addClass("past");
+    else if (currentTime > hoursArray[i]) {
+      dayPlannerIDArray[i].addClass("past");
     }
 
   };
+};
+
+// When the save button is pressed, it will get the parent div ID and the value of the text area. Then it will save it to local storage under ID name
+$(".saveBtn").on("click", function(){
+
+  var parentID = $(this).closest("div").attr("id");
+  var parentChildTextBox = $(this).closest("div").children("textarea").val();
+
+  for (var j = 0; j < dayPlannerIDArray.length; j++) {
+
+    var parentArrayID = dayPlannerIDArray[j].attr("id")
+
+    if (parentID === parentArrayID) {
+
+      localStorage.setItem(parentID, JSON.stringify(parentChildTextBox));
+
+    }
+  }
+});
+
+// When the page loads, then it will set the stored events to each ID
+function getStoredEvents() {
+
+  for (var k = 0; k < dayPlannerIDArray.length; k++) {
+
+    var oldParentArrayID = dayPlannerIDArray[k].attr("id")
+    var plannerText = JSON.parse(localStorage.getItem(oldParentArrayID));
+
+    dayPlannerIDArray[k].children("textarea").val(plannerText);
+
+  }
 };
