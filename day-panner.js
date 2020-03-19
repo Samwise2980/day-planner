3f838ee5df68f0b538803c64cb3c80dec7eea3d4
Samwise <samuelfoster0@gmail.com>
2020-03-18 21:46:38 -0700

    Time slots will adjust classes dynamically with the current day and time.

--- day-panner.js
+++ day-panner.js
@@ -2,12 +2,6 @@ moment().format();
 
 // GIVEN I am using a daily planner to create a schedule
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
 // WHEN I click into a timeblock
 // THEN I can enter an event
 
@@ -42,13 +36,13 @@ moment().format();
 // Get the text string for each hour from local storage
 
 
-var currentTime = moment().format("h:mm:ss");
-var hours = [{hourStart: "9:00:00", hourEnd: "9:59:59"}, {hourStart: "10:00:00", hourEnd: "10:59:59"}, {hourStart: "11:00:00", hourEnd: "11:59:59"}, {hourStart: "12:00:00", hourEnd: "12:59:59"}, {hourStart: "13:00:00", hourEnd: "13:59:59"}, {hourStart: "14:00:00", hourEnd: "14:59:59"}, {hourStart: "15:00:00", hourEnd: "15:59:59"}, {hourStart: "16:00:00", hourEnd: "16:59:59"}, {hourStart: "17:00:00", hourEnd: "17:59:59"}]
+var currentTime = moment().format("HH");
+var hours = [09, 10, 11, 12, 13, 14, 15, 16, 17]
+var dayPlannerID = [$("#hour-9"), $("#hour-10"), $("#hour-11"), $("#hour-12"), $("#hour-13"), $("#hour-14"), $("#hour-15"), $("#hour-16"), $("#hour-17"),]
 
 // Execute functions
 currentDay();
 hourColorSet();
-
 function currentDay(){
 
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
@@ -59,8 +53,21 @@ $(".saveBtn").on("click", function(){
   alert("Yep, still here!")
 });
 
+
 function hourColorSet(){
   for (i = 0; i < hours.length; i++) {
+
+    if (currentTime == hours[i]) {
+      dayPlannerID[i].addClass("present");
+    } 
     
-  }
+    else if (currentTime < hours[i]) {
+      dayPlannerID[i].addClass("future");
+    }
+    
+    else if (currentTime > hours[i]) {
+      dayPlannerID[i].addClass("past");
+    }
+
+  };
 };
